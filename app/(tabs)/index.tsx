10d8352fdecd79ccdb3cf5650c9f7c9c0dfecf23
pakhtoon khan <134305964+Pakhtoonkhanafridi@users.noreply.github.com>
2025-05-20 11:28:28 +0500

    Update index.tsx
    Remove Android making

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! pakhtoon Khan afridi making Android App</ThemedText>
+        <ThemedText type="title">Welcome! pakhtoon Khan afridi </ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
